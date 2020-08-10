2310c3351b249569022d0f164a4056cae0dd8b49
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-08-10 14:58:59 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.bot import Bot
 
 
-API_TOKEN = '1204400024:AAFWMQzwqI76urRI72WkUBfbnCMJcWRyxbs
+API_TOKEN = '1204400024:AAFWMQzwqI76urRI72WkUBfbnCMJcWRyxbs'
 
 if __name__ == '__main__':
     bot = Bot(API_TOKEN)
